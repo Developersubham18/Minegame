507e56cb8a7e40244bdee06c5dc3c08ebb9132cf
Subham Kumar Jha <104671103+Developersubham18@users.noreply.github.com>
2025-12-09 00:58:18 +0530

    Update App.js
    change build to made to see deployment time in cicd pipeline i.e 14s

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <footer className="flex justify-center items-center w-[100%] md:mx-auto  bottom-1 max-w-[1200px] ">
         {" "}
         <h1 className="font-semibold text-xl text-white">
-          Built with ❤️ by{" "}
+          Made with ❤️ by{" "}
           <a
             className="ml-2 underline text-primary "
             href="https://ashish4.vercel.app"
