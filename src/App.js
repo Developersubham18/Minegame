f2eb4f8017d70089d04b6e4ffc955ee85b0ea833
Subham Kumar Jha <104671103+Developersubham18@users.noreply.github.com>
2025-12-09 22:57:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,8 @@ function App() {
     <div className="App ">
       <Main />
       <footer className="flex justify-center items-center w-[100%] md:mx-auto  bottom-1 max-w-[1200px] ">
-        {" "}
-        <h1 className="font-semibold text-xl text-white">
-          Made by{" "}
-          <a
-            className="ml-2 underline text-primary "
-            href="https://ashish4.vercel.app"
-            target="_blank"
-          >
-            Ashish
-          </a>
-              & deployed by Suzziea
-        </h1>
+      
+       
       </footer>
     </div>
   );
